2e34c3e6696778883c2ce5361239c975ef9aee22
Andrii <andriibozhko1@gmail.com>
2019-01-10 10:33:17 +0200

    upgrade

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,46 @@
 (function() {
   const createToolTip = function() {
-    const toolTip = document.createElement('div');
-    toolTip.classList.add('tooltip');
+    const toolTip = document.createElement("div");
+    toolTip.classList.add("tooltip");
     document.body.appendChild(toolTip);
 
-    window.addEventListener('mousemove', function(e){
-      toolTip.style.left = e.screenX + 'px';
-      toolTip.style.top = e.screenY - 130 + 'px';
-      toolTip.innerHTML = 'X :'+ e.screenX + ' Y: ' + e.screenY
-    })
-    
-  }
+    function throttle(func, ms) {
+      var isThrottled = false;
+        let savedArgs;
+        let savedThis;
+
+      function wrapper() {
+        if (isThrottled) {                                               
+          savedArgs = arguments;
+          savedThis = this;
+          return;
+        }
+
+        func.apply(this, arguments);
+
+        isThrottled = true;
+
+        setTimeout(function() {
+          isThrottled = false;
+          if (savedArgs) {       
+            wrapper.apply(this,savedArgs)       
+            savedArgs = null;          
+          }
+        }, ms);
+      }
+
+      return wrapper;
+    }
+
+    let showToolTip =  function(X,Y) {
+      toolTip.style.left = X + "px";
+      toolTip.style.top = Y - 130 + "px";
+      toolTip.innerHTML = "X :" + X + " Y: " + Y;
+    }
+    showToolTip = throttle(showToolTip,500);
+    window.addEventListener("mousemove", function(e) {
+      showToolTip(e.screenX,e.screenY);
+    });
+  };
   createToolTip();
-}())
+}());
